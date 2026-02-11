734eb5812d957c5afcddfae3995763bc14f805e7
Ngô Phú <ngophu02.lv@gmail.com>
2026-02-12 01:03:31 +0700

    Add Login form + darkmode

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function LoginPage() {
   return (
@@ -10,7 +12,18 @@ export default function LoginPage() {
         </CardHeader>
 
         <CardContent>
-            <Button className="w-full" variant={"outline"}>Đăng nhập</Button>
+            <div className="grid gap-3">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="email@gmail.com"/>
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="password">Mật khẩu</Label>
+                <Input id="password" type="password" placeholder="Nhập mật khẩu của bạn"/>
+              </div>
+              <Button>Đăng nhập</Button>
+            </div>
         </CardContent>
     </Card>
   );
